b9d889c5d7e6bfadb686e5680ba59dda75d7f6f9
asif-007 <asif.sadi00@gmail.com>
2018-11-10 11:53:24 +0600

    version 1.0

---
+++ src/main/java/com/asif/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.asif.springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
